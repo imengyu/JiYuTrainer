58a39ec5ff3c4a644a01293b4841a94f30e341d4
717021 <32213395+717021@users.noreply.github.com>
2019-05-13 12:05:37 +0800

    删除不需要的文件

--- Release/JiYuTrainerUInstall.bat
+++
@@ -1,7 +0,0 @@
-del /F /Q E:\Programming\WinProjects\JiYuTrainer\Release\JiYuTrainer.bat
-del /F /Q E:\Programming\WinProjects\JiYuTrainer\Release\JiYuTrainerUI.dll
-del /F /Q E:\Programming\WinProjects\JiYuTrainer\Release\JiYuTrainerHooks.dll
-del /F /Q E:\Programming\WinProjects\JiYuTrainer\Release\JiYuTrainerDriver.sys
-del /F /Q E:\Programming\WinProjects\JiYuTrainer\Release\JiYuTrainerUpdater.dll
-del /F /Q E:\Programming\WinProjects\JiYuTrainer\Release\sciter.dll
-del %0
